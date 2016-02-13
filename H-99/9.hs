18d2f41e55c38e08edf061353fe94c09e94a1794
kamelzcs <kamelzcs@gmail.com>
2016-02-13 11:58:52 +0900

    added 9.hs

---
+++ H-99/9.hs
@@ -0,0 +1,5 @@
+pack [] = [[]]
+pack [x] = [[x]]
+pack (x : ys@(y : _)) = let ps = pack ys
+                            as = if (x /= y) then ([[x]] ++ ps) else ([x : head ps] ++ (tail ps))
+                        in as
