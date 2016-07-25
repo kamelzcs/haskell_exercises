0f4957515778d8bff023fc04c1a1a9ec46f675c9
Shuai Zhao <zhaoshua@amazon.co.jp>
2016-07-25 17:02:40 +0900

    changed isPrime function

--- Project_Euler/58.hs
+++ Project_Euler/58.hs
@@ -5,12 +5,15 @@ diag = 3: 5: 7: 9 : zipWith (+) diag [10, 12..]
 toLength x = (x + 2) `div` 4 * 2 + 1
 
 primeCount = scanl1 (+) $ map (\x -> if (isPrime x) then 1 else 0) diag
-isPrime :: Integer -> Bool
-isPrime x = go x 2
-  where
-  go x y
-    | square y > x = True
-    | ((== 0) (x `mod` y)) = False
-    | otherwise = go x (1 + y)
+
+primes = 2 : filter isPrime [3, 5..]
+
+isPrime x = null $ tail $ primeFactors x primes
+
+primeFactors n (p : ps)
+    | p * p > n = [n]
+    | n `mod` p == 0 = p : primeFactors (n `div` p) (p : ps)
+    | otherwise = primeFactors n ps
+
 
 main = print $ toLength $ snd $ head $ dropWhile (\(p, n)-> 10 * p >= n) $ zip primeCount [2, 3..]
