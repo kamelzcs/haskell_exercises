05168483cf9ec910e7ef53103e3d441d975644aa
kamelzcs <kamelzcs@gmail.com>
2016-07-28 08:29:29 +0900

    added 64

--- Project_Euler/64.hs
+++ Project_Euler/64.hs
@@ -1,9 +1,13 @@
+import Data.List
+
+sqrtFloor x = truncate (sqrt (fromIntegral x))
+
 frac :: Integer -> [(Integer, (Integer, Integer))]
-frac x = takeWhile ((/=2) . fst) iterate f (a0, (0, 1))
-  where a0 = truncate (sqrt (fromIntegral x))
+frac x = takeWhile ((/= 2 * (sqrtFloor x)) . fst) $ iterate f (a0, (0, 1))
+  where a0 = sqrtFloor x
         f (a, (m, d)) = (a1, (m1, d1))
           where m1 = d * a - m
                 d1 = (x - m1 * m1) `div` d
                 a1 = (a0 + m1) `div` d1
 
-main = print $ length $ filter (even . length) $ map frac [2..10000]
+main = print $ length $ filter (odd . length) $ map frac $ [2..10000] \\ map (^2) [2..100]
