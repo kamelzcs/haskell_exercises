b99b20567ccaad51e3862645ac9f5e67307fb154
kamelzcs <kamelzcs@gmail.com>
2016-07-30 11:53:52 +0900

    added 70

---
+++ H-99/70.hs
@@ -0,0 +1,29 @@
+import Control.Monad.State
+
+data Tree a = Node a [Tree a]
+        deriving (Eq, Show)
+
+parses :: State String [Tree Char]
+parses = do
+  str <- get
+  let c = head str
+  case c of
+    '^' -> do
+        put (tail str)
+        return []
+    _ -> do
+        let (t, s) = runState parse str
+            (ts, s1) = runState parses s
+        put s1
+        return $ t : ts
+
+parse :: State String (Tree Char)
+parse = do
+  str <- get
+  let c = head str
+      (ts, s) = runState parses (tail str)
+  put s
+  return $ Node c ts
+
+stringToTree :: String -> Tree Char
+stringToTree = fst . runState parse
