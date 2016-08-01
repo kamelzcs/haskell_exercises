dd2d59dc70901e2a59cfa0a0a49cab0679e03ad7
kamelzcs <kamelzcs@gmail.com>
2016-08-01 23:09:02 +0900

    added grains.hs

--- exercism/haskell/grains/src/Grains.hs
+++ exercism/haskell/grains/src/Grains.hs
@@ -1,8 +1,10 @@
 module Grains (square, total) where
 
+import Data.Maybe
+
 square :: Integer -> Maybe Integer
 square n
  | n > 64 || n <= 0 = Nothing
  | otherwise = Just $ iterate (*2) 1 !! (fromIntegral n - 1)
 
-total = sum $ (map ((maybe 0 id) . square) [1..64])
+total = sum $ (map (fromMaybe 0 . square) [1..64])
