7a5957916e944d3d4eac4a652780bd74f4beeafe
kamelzcs <kamelzcs@gmail.com>
2016-08-02 22:42:11 +0900

    changed bob

--- exercism/haskell/grains/src/Grains.hs
+++ exercism/haskell/grains/src/Grains.hs
@@ -7,4 +7,4 @@ square n
  | n > 64 || n <= 0 = Nothing
  | otherwise = Just $ iterate (*2) 1 !! (fromIntegral n - 1)
 
-total = sum $ (map (fromMaybe 0 . square) [1..64])
+total = sum $ (map (fromJust . square) [1..64])
