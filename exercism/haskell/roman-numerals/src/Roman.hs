e0e5093d353b5764a6780a805002eeef913fde81
kamelzcs <kamelzcs@gmail.com>
2016-08-10 08:31:15 +0900

    still wrong for roman

---
+++ exercism/haskell/roman-numerals/src/Roman.hs
@@ -0,0 +1,13 @@
+module Roman (numerals) where
+
+import Prelude hiding (lookup)
+import Data.Map(fromList, lookup, keys)
+import Data.Maybe
+import Data.List(foldl')
+
+
+dict = fromList [(1000, "M"), (500, "D"), (400, "CD"), (100, "C"), (50, "L"), (40, "XL"), (10, "X"), (5, "V"), (4, "IV"), (1, "I")]
+
+
+numerals :: Int -> String
+numerals n = fst $ foldl' (\(s, t) k -> (s ++ (concat $ replicate (t `div` k) $ fromJust $ lookup k dict), t - (t `div` k) * k)) ("", n) $ reverse (keys dict)
